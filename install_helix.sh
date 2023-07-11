2c38ca5e49b40ada705f33042dd198e62ec680f1
ubermanu <manu@paca.happy-dev.fr>
2023-07-11 11:39:44 +0200

    update helix conf

--- install_helix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -xe 
-
-yay -S helix
-yay -S marksman codelldb-bin rust-analyzer
-
-# node based language servers
-yay -Sdd vscode-css-languageserver vscode-html-languageserver vscode-json-languageserver typescript-language-server bash-language-server svelte-language-server yaml-language-server dockerfile-language-server
