cb314a078e30bd876393aa6819d7e3fd41433acd
ubermanu <manu@paca.happy-dev.fr>
2024-01-31 11:57:25 +0100

    update config

--- dot_screenlayout/private_executable_default.sh
+++ dot_screenlayout/private_executable_default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-2 --off --output DP-3 --off --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-4 --off --output DP-5 --off --output None-1-1 --off
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 2560x246 --rotate normal --output DP-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --off --output HDMI-1 --off --output DP-4 --off --output DP-5 --off --output None-1-1 --off
